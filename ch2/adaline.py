33d8bfdf3298eb381ba9749a2d7e5da22cbf7a10
roberto montalti <roberto.montalti00@gmail.com>
2023-02-25 23:20:31 +0100

    Initial commit

---
+++ ch2/adaline.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class AdalineGD:
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.1, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+
+            """
+                for w_j in range(self.w_.shape[0]):
+                    self.w_[w_j] += self.eta * (2.0 * (X[:, w_j]*errors)).mean()
+            """
+
+            """ maps to: w := w + (-eta * dL/dWj)"""
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            """ maps to: b := b + (-eta * dL/db)"""
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """Linear activation function"""
+        return X
+
+    def predict(self, X):
+        """1 or 0 with a threshold of 0.5"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
---
+++ ch2/iris.py
@@ -0,0 +1,116 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+#custom perceptron
+from perceptron import Perceptron
+from adaline import AdalineGD
+from adaline_sgd import AdalineSGD
+
+s = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+df = pd.read_csv(s, header=None, encoding="utf-8")
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == "Iris-setosa", 0, 1)
+X = df.iloc[0:100, [0, 2]].values
+#plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="Setosa")
+#plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="s", label="Versicolor")
+#plt.xlabel("Sepal length [cm]")
+#plt.xlabel("Petal length [cm]")
+#plt.legend(loc="upper left")
+#plt.show()
+
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("No. updates")
+plt.show()
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                y=X[y == cl, 1],
+                alpha=0.8,
+                c=colors[idx],
+                marker=markers[idx],
+                label=f'Class {cl}',
+                edgecolor='black')
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel("Sepal length [cm]")
+plt.ylabel("Petal length [cm]")
+plt.legend(loc="upper left")
+plt.show()
+ 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel("Epochs")
+ax[0].set_ylabel("log(Mean squared error)")
+ax[0].set_title("Adaline - Learning rate 0.1")
+ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) + 1), np.log10(ada2.losses_), marker='o')
+ax[1].set_xlabel("Epochs")
+ax[1].set_ylabel("Mean squared error")
+ax[1].set_title("Adaline - Learning rate 0.0001")
+plt.show()
+
+# Standardization, aka feature scaling, once again...
+# Recall that: std (aka standard deviant) is the amount of dispersion
+#   in a set of values, a high value means a generally low chance of finding
+#   values around the set mean, a low one guess what? a high chance.
+#   Standardization will group all the data closer to zero and lower it's std
+#   (to be precise, the set variance will we exactly 1)
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada_gd = AdalineGD(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada_gd)
+plt.title("Adaline - Gradient descent")
+plt.xlabel("Sepal length [standardization]")
+plt.xlabel("Petal length [standardization]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Mean squared error")
+plt.tight_layout()
+plt.show()
+
+ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada_sgd.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada_sgd)
+plt.title("Adaline - Stochastic gradient descent")
+plt.xlabel("Sepal length [standardized]")
+plt.ylabel("Petal length [standardized]")
+plt.legend(loc="upper left")
+plt.show()
+plt.plot(range(1, len(ada_sgd.losses_) + 1), ada_sgd.losses_, marker="o")
+plt.xlabel("Epcohs")
+plt.ylabel("Average loss")
+plt.tight_layout()
+plt.show()
