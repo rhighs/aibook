25f2886696f42fe9fc890b8b1d7d38425ebfe717
roberto montalti <roberto.montalti00@gmail.com>
2023-03-07 12:27:35 +0100

    The kernel trick

--- ch3/main.py
+++ ch3/main.py
@@ -160,4 +160,30 @@ Non linearly separable classes -> map them onto a 3d space -> fit a linear svm m
 (aka find the decision boundary) -> map the decision boundary back into the original dimension
 """
 
+from sklearn.svm import SVC
+
+svm = SVC(kernel="rbf", random_state=1, gamma=0.1, C=10.)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+svm = SVC(kernel="rbf", random_state=1, gamma=0.1, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel("Petal length [standardized]")
+plt.ylabel("Petal width [standardized]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+svm = SVC(kernel="rbf", random_state=1, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel("Petal length [standardized]")
+plt.ylabel("Petal width [standardized]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
 
