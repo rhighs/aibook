97f570dd5c109646bc571edc5e5ba8de16aee62b
roberto montalti <roberto.montalti00@gmail.com>
2023-03-07 09:21:58 +0100

    Regularization visualization

---
+++ ch3/loss.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+def loss_1(z):
+    return -np.log(sigmoid(z))
+
+def loss_0(z):
+    return -np.log(1 - sigmoid(z))
+
+z = np.arange(-10, 10, 0.1)
+sigma_z = sigmoid(z)
+c1 = [loss_1(x) for x in z]
+plt.plot(sigma_z, c1, label="L(W, b) if y=1")
+c0 = [loss_0(x) for x in z]
+plt.plot(sigma_z, c0, label="L(W, b) if y=0")
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+plt.xlabel("$\sigma(z)$")
+plt.ylabel("L(w, b)")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.show()
--- ch3/main.py
+++ ch3/main.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
+from logistic_regression_gd import LogisticRegressionGD
+
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02): # setup marker generator and color map
     markers = ('o', 's', '^', 'v', '<')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -72,6 +74,53 @@ X_combined_std= np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
 plt.xlabel("Petal length [standardized]")
-plt.xlabel("Petal width [standardized]")
+plt.ylabel("Petal width [standardized]")
 plt.tight_layout()
 plt.show()
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train) == 1]
+y_train_01_subset = y_train[(y_train == 0) | (y_train) == 1]
+lrgd = LogisticRegressionGD(eta=0.3,
+                            n_iter=1000,
+                            random_state=1)
+
+lrgd.fit(X_train_01_subset, y_train_01_subset)
+plot_decision_regions(X=X_train_01_subset, y=y_train_01_subset, classifier=lrgd)
+plt.xlabel("Petal length [standardized]")
+plt.ylabel("Petal width [standardized]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(C=100.0, solver="lbfgs", multi_class="ovr")
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+plt.xlabel("Petal length [standardized]")
+plt.ylabel("Petal width [standardized]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+prediction = lr.predict_proba(X_test_std[:3, :]).argmax(axis=1)
+print("Classes predicted", prediction)
+
+# Regularization and feature scaling
+
+weights, params = [], []
+
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10.0 ** c, multi_class="ovr")
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10.0**c)
+weights = np.array(weights)
+plt.plot(params, weights[:, 0], label="Petal length")
+plt.plot(params, weights[:, 1], linestyle="--", label="Petal width")
+plt.ylabel("Weight coefficient")
+plt.xlabel("X")
+plt.legend(loc="upper left")
+plt.xscale("log")
+plt.show()
+
