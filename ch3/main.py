1d09184237d6e365bea7402eed0f713c0c7b43ea
roberto montalti <roberto.montalti00@gmail.com>
2023-03-24 17:40:44 +0100

    `Chapter 3`: regression trees, random forests and knns

--- ch3/main.py
+++ ch3/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import Perceptron
@@ -6,9 +5,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
 
 from logistic_regression_gd import LogisticRegressionGD
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02): # setup marker generator and color map
     markers = ('o', 's', '^', 'v', '<')
@@ -181,6 +182,103 @@ plt.show()
 svm = SVC(kernel="rbf", random_state=1, gamma=100.0, C=1.0)
 svm.fit(X_train_std, y_train)
 plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel("Petal length [standardized]")
+plt.ylabel("Petal width [standardized]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+
+def visualize_entropy_criterion():
+    """
+    By executing this portion we can visualize how a dependending on the
+    "uniformity" of class distribution, the entropy varies.
+    """
+    def entropy(p: float) -> float:
+        return -p * np.log2(p) - (1 - p) * np.log2((1-p))
+    
+    __x = np.arange(0.0, 1.0, 0.01)
+    ent = [entropy(p) if p != 0 else None for p in __x]
+    plt.ylabel("Entropy")
+    plt.xlabel("Class-membership probability p(i=1)")
+    plt.plot(__x, ent)
+    plt.show()
+visualize_entropy_criterion()
+
+def visualize_criterion_comparisons():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    def gini(p):
+        return p * (1-p) + (1 - p) * (1 - (1 - p))
+    def entropy(p: float) -> float:
+        return -p * np.log2(p) - (1 - p) * np.log2((1-p))
+    def error(p):
+        return 1 - np.max([p, 1 - p])
+
+    x = np.arange(0.0, 1.0, 0.01)
+    ent = [entropy(p) if p !=0 else None for p in x]
+    sc_ent = [e * 0.5 if e else None for e in ent]
+    err = [error(i) for i in x]
+    fig = plt.figure()
+    ax = plt.subplot(111)
+
+    for i, lab, ls, c in zip([ent, sc_ent, gini(x), err],
+                             ["Entropy", "Entropy (scaled)", "Gini impurity", "Misclassification error"],
+                             ["-", "-", "--", "-."],
+                             ["black", "lightgray", "red", "green", "cyan"]
+                             ):
+        line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+    ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=5, fancybox=True, shadow=False)
+    ax.axhline(y=0.5, linewidth=1, color="k", linestyle="--")
+    ax.axhline(y=1.0, linewidth=1, color="k", linestyle="--")
+    plt.ylim([0, 1.1])
+    plt.xlabel("p(i=1)")
+    plt.ylabel("impurity index")
+    plt.show()
+
+visualize_criterion_comparisons()
+
+from sklearn.tree import DecisionTreeClassifier
+
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y
+        )
+
+tree_model = DecisionTreeClassifier(criterion="gini", max_depth=4, random_state=1)
+tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree_model, test_idx=range(105, 150))
+
+plt.xlabel("Petal length [cm]")
+plt.ylabel("Petal width [cm]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+from sklearn import tree
+feature_names = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
+tree.plot_tree(tree_model, feature_names=feature_names, filled=True)
+plt.show()
+
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators=25,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+plt.xlabel("Petal length [cm]")
+plt.ylabel("Petal width [cm]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric="minkowski")
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105, 150))
+
 plt.xlabel("Petal length [standardized]")
 plt.ylabel("Petal width [standardized]")
 plt.legend(loc="upper left")
