78c8b0225e808ff554fc3496b57ed33d5bb5c60b
Roberto Montalti <roberto.montalti00@gmail.com>
2023-04-25 18:39:21 +0200

    make_pipeline for scaling -> decomposition -> regression

---
+++ ch6/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+ 'machine-learning-databases'
+ '/breast-cancer-wisconsin/wdbc.data',
+ header=None)
+
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(le.classes_)
+
+X_train, X_test, y_train, y_test = \
+        train_test_split(X, y,
+                         test_size=0.20,
+                         stratify=y,
+                         random_state=1)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        PCA(n_components=2),
+                        LogisticRegression())
+
+pipe_lr.fit(X_train, y_train)
+y_pred = pipe_lr.predict(X_test)
+test_acc = pipe_lr.score(X_test, y_test)
+print(f"Test accuracy: {test_acc:.3f}")
