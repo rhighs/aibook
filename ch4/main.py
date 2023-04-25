7c2f7a1a03dd7b4446afdd16f1564cfe3b298a6f
Roberto Montalti <roberto.montalti00@gmail.com>
2023-04-25 17:48:50 +0200

    End of chapter 4

--- ch4/main.py
+++ ch4/main.py
@@ -120,4 +120,92 @@ def features_onto_same_scale():
 
 
 def sequential_feature_selection():
+    from sklearn import datasets
+    from sklearn.base import clone
+    from itertools import combinations
+    import numpy as np
+    from sklearn.metrics import accuracy_score
+    from sklearn.model_selection import train_test_split
 
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y
+        )
+
+    from sklearn.preprocessing import StandardScaler
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(X_train)
+    X_test_std = stdsc.fit_transform(X_test)
+
+
+    class SBS:
+        def __init__(self, estimator, k_features,
+                     scoring=accuracy_score,
+                     test_size=0.25, random_state=1):
+            self.scoring = scoring
+            self.estimator = clone(estimator)
+            self.k_features = k_features
+            self.test_size = test_size
+            self.random_state = random_state
+
+        def fit(self, X, y):
+            X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=self.test_size,
+                             random_state=self.random_state)
+
+            dim = X_train.shape[1]
+            self.indices_ = tuple(range(dim))
+            self.subsets_ = [self.indices_]
+            score = self._calc_score(X_train, y_train,
+                                     X_test, y_test, self.indices_)
+
+            self.scores_ = [score]
+            while dim > self.k_features:
+                scores = []
+                subsets = []
+
+                # Try every combination without a certain feature column x'
+                for p in combinations(self.indices_, r=dim - 1):
+                    score = self._calc_score(X_train, y_train, 
+                                             X_test, y_test, p)
+                    scores.append(score)
+                    subsets.append(p)
+                
+
+                best = np.argmax(scores)
+                self.indices_ = subsets[best]
+                self.subsets_.append(self.indices_)
+                dim -= 1
+
+                self.scores_.append(scores[best])
+            self.k_score_ = self.scores_[-1]
+            return self
+
+        def transform(self, X):
+            return X[:, self.indices_]
+        
+        def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+            self.estimator.fit(X_train[:, indices], y_train)
+            y_pred = self.estimator.predict(X_test[:, indices])
+            score = self.scoring(y_test, y_pred)
+            return score
+
+    import matplotlib.pyplot as plt
+    from sklearn.neighbors import KNeighborsClassifier
+
+    knn = KNeighborsClassifier(n_neighbors=5)
+    sbs = SBS(knn, k_features=1)
+    sbs.fit(X_train_std, y_train)
+    k_feat = [len(k) for k in sbs.subsets_]
+    plt.plot(k_feat, sbs.scores_, marker='o')
+    plt.ylim([0.7, 1.02])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of features')
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+sequential_feature_selection()
