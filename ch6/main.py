cfd0e8b21c6fe08321ef1a0754d3391f2b428a00
Roberto Montalti <roberto.montalti00@gmail.com>
2023-07-02 15:02:35 +0200

    CH6: learning and validation curves

--- ch6/main.py
+++ ch6/main.py
@@ -51,9 +51,118 @@ mean_acc = np.mean(scores)
 std_acc = np.std(scores)
 print(f"\nCV Accuracy: {mean_acc:.3f} +/- {std_acc:.3f}")
 
-# Less verbose way of the abovei implementation of cross validation score evaluation
+# Less verbose way of the above implementation of cross validation score evaluation
 from sklearn.model_selection import cross_val_score
 
 scores = cross_val_score(estimator=pipe_lr, X=X_train, y=y_train, cv=10, n_jobs=1)
 print(f"\nCV Accuracy scores: {scores}")
 print(f"CV accuracy: {np.mean(scores):.3f} +/- {np.std(scores):.3f}")
+
+# Learning curves and validation curves
+# Refresher here:
+# https://datascience.stackexchange.com/questions/62303/difference-between-learning-curve-and-validation-curve?newreg=9c1e5ec325444eaf96b167dd2b554902
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve, validation_curve
+
+pipe_lr = make_pipeline(
+    StandardScaler(), LogisticRegression(penalty="l2", max_iter=10000)
+)
+
+train_sizes, train_scores, test_scores = learning_curve(
+    estimator=pipe_lr,
+    X=X_train,
+    y=y_train,
+    train_sizes=np.linspace(0.1, 1.0, 10),
+    cv=10,
+    n_jobs=1,
+)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(
+    train_sizes,
+    train_mean,
+    color="blue",
+    marker="o",
+    markersize=5,
+    label="Training accuracy",
+)
+plt.fill_between(
+    train_sizes,
+    train_mean + train_std,
+    train_mean - train_std,
+    alpha=0.15,
+    color="blue",
+)
+plt.plot(
+    train_sizes,
+    test_mean,
+    color="green",
+    linestyle="--",
+    marker="s",
+    markersize=5,
+    label="Validation accuracy",
+)
+plt.fill_between(
+    train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color="green"
+)
+
+plt.grid()
+plt.xlabel("Number of training examples")
+plt.ylabel("Accuracy")
+plt.legend(loc="lower right")
+plt.ylim([0.8, 1.03])
+plt.show()
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+    estimator=pipe_lr,
+    X=X_train,
+    y=y_train,
+    param_name="logisticregression__C",
+    param_range=param_range,
+    cv=10,
+)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(
+    param_range,
+    train_mean,
+    color="blue",
+    marker="o",
+    markersize=5,
+    label="Training accuracy",
+)
+plt.fill_between(
+    param_range,
+    train_mean + train_std,
+    train_mean - train_std,
+    alpha=0.15,
+    color="blue",
+)
+plt.plot(
+    param_range,
+    test_mean,
+    color="green",
+    linestyle="--",
+    marker="s",
+    markersize=5,
+    label="Validation accuracy",
+)
+plt.fill_between(
+    param_range, test_mean + test_std, test_mean - test_std, alpha=0.15, color="green"
+)
+plt.grid()
+plt.xscale("log")
+plt.xlabel("Param C")
+plt.ylabel("Accuracy")
+plt.legend(loc="lower right")
+plt.ylim([0.8, 1.03])
+plt.show()
