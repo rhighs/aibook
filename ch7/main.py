78a95d36d4d63ef76fcb306793eefd6c38b3fe73
Roberto Montalti <roberto.montalti00@gmail.com>
2023-08-10 16:26:46 +0200

    CH7: adaboost examples, showing discretely high variance and complexity

--- ch7/main.py
+++ ch7/main.py
@@ -3,6 +3,41 @@ from scipy.special import comb
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import pandas as pd
+
+df_wine = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
+    header=None,
+)
+df_wine.columns = [
+    "Class label",
+    "Alcohol",
+    "Malic acid",
+    "Ash",
+    "Alcalinity of ash",
+    "Magnesium",
+    "Total phenols",
+    "Flavanoids",
+    "Nonflavanoid phenols",
+    "Proanthocyanins",
+    "Color intensity",
+    "Hue",
+    "OD280/OD315 of diluted wines",
+    "Proline",
+]
+
+df_wine = df_wine[df_wine["Class label"] != 1]
+y = df_wine["Class label"].values
+X = df_wine[["Alcohol", "OD280/OD315 of diluted wines"]].values
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1, stratify=y
+)
 
 
 def ensemble_error(n_classifier, error):
@@ -183,42 +218,6 @@ def majority_voting_classifier():
 
 
 def bagging_ensembles_classifier():
-    import pandas as pd
-
-    df_wine = pd.read_csv(
-        "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
-        header=None,
-    )
-    df_wine.columns = [
-        "Class label",
-        "Alcohol",
-        "Malic acid",
-        "Ash",
-        "Alcalinity of ash",
-        "Magnesium",
-        "Total phenols",
-        "Flavanoids",
-        "Nonflavanoid phenols",
-        "Proanthocyanins",
-        "Color intensity",
-        "Hue",
-        "OD280/OD315 of diluted wines",
-        "Proline",
-    ]
-
-    df_wine = df_wine[df_wine["Class label"] != 1]
-    y = df_wine["Class label"].values
-    X = df_wine[["Alcohol", "OD280/OD315 of diluted wines"]].values
-
-    from sklearn.preprocessing import LabelEncoder
-    from sklearn.model_selection import train_test_split
-
-    le = LabelEncoder()
-    y = le.fit_transform(y)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=1, stratify=y
-    )
-
     from sklearn.ensemble import BaggingClassifier
     from sklearn.tree import DecisionTreeClassifier
 
@@ -287,4 +286,79 @@ def bagging_ensembles_classifier():
     plt.show()
 
 
-bagging_ensembles_classifier()
+def ada_boost_classification():
+    y = np.array([1, 1, 1, -1, -1, -1, 1, 1, 1, -1])
+    yhat = np.array([1, 1, 1, -1, -1, -1, -1, -1, -1, -1])
+    correct = y == yhat
+    weights = np.full(10, 0.1)
+    print("weights =", weights)
+    epsilon = np.mean(~correct)
+    print("epsilon =", epsilon)
+    alpha_j = 0.5 * np.log((1 - epsilon) / epsilon)
+    print("alpha_j =", alpha_j)
+    update_if_correct = 0.1 * np.exp(-alpha_j * 1 * 1)
+    print("updated correct =", update_if_correct)
+    update_if_wrong = 0.1 * np.exp(-alpha_j * 1 * -1)
+    print("updated wrong =", update_if_wrong)
+    updated_weights = np.multiply(
+        weights, np.exp(np.multiply(-alpha_j, np.multiply(y, yhat)))
+    )
+    print("updated weights =", updated_weights)
+    norm_w = updated_weights / np.sum(updated_weights)
+    print("normalized weights =", norm_w)
+
+    from sklearn.metrics import accuracy_score
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+
+    tree = DecisionTreeClassifier(criterion="entropy", random_state=1, max_depth=1)
+    ada = AdaBoostClassifier(
+        estimator=tree, n_estimators=500, learning_rate=0.1, random_state=1
+    )
+    tree = tree.fit(X_train, y_train)
+    y_train_pred = tree.predict(X_train)
+    y_test_pred = tree.predict(X_test)
+    tree_train = accuracy_score(y_train, y_train_pred)
+    tree_test = accuracy_score(y_test, y_test_pred)
+    print(f"Decision tree train/test accuracies {tree_train:.3f}/{tree_test:.3f}")
+
+    ada = ada.fit(X_train, y_train)
+    y_train_pred = ada.predict(X_train)
+    y_test_pred = ada.predict(X_test)
+    ada_train = accuracy_score(y_train, y_train_pred)
+    ada_test = accuracy_score(y_test, y_test_pred)
+    print(f"Ada boosttrain/test accuracies {ada_train:.3f}/{ada_test:.3f}")
+
+    x_min = X_train[:, 0].min() - 1
+    x_max = X_train[:, 0].max() + 1
+    y_min = X_train[:, 1].min() - 1
+    y_max = X_train[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+    f, axarr = plt.subplots(1, 2, sharex="col", sharey="row", figsize=(8, 3))
+    for idx, clf, tt in zip([0, 1], [tree, ada], ["Decision tree", "AdaBoost"]):
+        clf.fit(X_train, y_train)
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+        axarr[idx].scatter(
+            X_train[y_train == 0, 0], X_train[y_train == 0, 1], c="blue", marker="^"
+        )
+        axarr[idx].scatter(
+            X_train[y_train == 1, 0], X_train[y_train == 1, 1], c="green", marker="o"
+        )
+        axarr[idx].set_title(tt)
+        axarr[0].set_ylabel("Alcohol", fontsize=12)
+    plt.tight_layout()
+    plt.text(
+        0,
+        -0.2,
+        s="OD280/OD315 of diluted wines",
+        ha="center",
+        va="center",
+        fontsize=12,
+        transform=axarr[1].transAxes,
+    )
+    plt.show()
+
+
+ada_boost_classification()
