8f82b74acd2186c1d091c09eab143be0b78f25c5
Roberto Montalti <roberto.montalti00@gmail.com>
2023-09-18 22:04:15 +0200

    CH8: lda example, review topics

--- ch8/main.py
+++ ch8/main.py
@@ -179,3 +179,38 @@ def outofcore_example():
     print(f"Accuracy: {clf.score(X_test, y_test)}")
 
     clf.partial_fit(X_test, y_test)
+
+
+def LDA_example():
+    df = pd.read_csv("movie_data.csv", encoding="utf-8")
+    df = df.rename(columns={"0": "review", "1": "sentiment"})
+
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    count = CountVectorizer(stop_words="english", max_df=0.1, max_features=5000)
+    X = count.fit_transform(df["review"].values)
+
+    from sklearn.decomposition import LatentDirichletAllocation
+
+    lda = LatentDirichletAllocation(
+        n_components=10, random_state=123, learning_method="batch"
+    )
+
+    X_topics = lda.fit_transform(X)
+
+    N_TOP_WORDS = 5
+    feature_names = count.get_feature_names_out()
+    for topic_idx, topic in enumerate(lda.components_):
+        print(f"Topic {(topic_idx + 1)}:")
+        print(
+            " ".join(
+                [feature_names[i] for i in topic.argsort()[: -N_TOP_WORDS - 1 : -1]]
+            )
+        )
+
+    # print a horror movie review
+    horror = X_topics[:, 5].argsort()[::-1]
+    for iter_idx, movie_idx in enumerate(horror[:3]):
+        print(f'\nHorror movie #{iter_idx}')
+        print(df['review'][movie_idx][:300], '...')
+LDA_example()
